dd6643b9c5f9a8e50d913ab8adce4b3d8977ee2e
Adnan-Ali-007 <79352611+Adnan-Ali-007@users.noreply.github.com>
2024-10-07 03:19:22 +0530

    ready to deploy

--- backend/server.js
+++ backend/server.js
@@ -56,4 +56,20 @@ io.on("connection", (socket) => {
     console.log(`User joined room: ${room}`);
   });
 
-  soc
+  socket.on('typing', (room) => socket.in(room).emit("typing"));
+  socket.on('stop typing', (room) => socket.in(room).emit("stop typing"));
+
+  socket.on('new message', (newMessageRecieved) => {
+    const chat = newMessageRecieved.chat;
+    if (!chat.users) return console.log("Chat users not defined");
+
+    chat.users.forEach((user) => {
+      if (user._id === newMessageRecieved.sender._id) return;
+      socket.in(user._id).emit("message received", newMessageRecieved);
+    });
+  });
+
+  socket.on('disconnect', () => {
+    console.log("User disconnected from socket.io");
+  });
+});
